d955b1d621fdd451ec06d88f1f75efbb751b5fe6
Tomás Cárdenas <tomicardenas96@gmail.com>
2025-04-27 09:53:18 -0300

    Modified authOptions google auth

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -6,12 +6,6 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      // Opcional: Si necesitas definir una callbackURL explícita:
-      authorization: {
-        params: {
-          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
-        },
-      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
